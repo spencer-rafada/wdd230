f8f098b03ae9740d8af2428b59178da194778104
spencer-rafada <spencer.byui@gmail.com>
2023-02-09 21:19:53 -0700

    added wind chill and optimized images

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -36,3 +36,25 @@ if (today.getDay() === 1 || today.getDay() === 2) {
     .querySelector(`header`)
     .insertAdjacentHTML(`beforebegin`, displayBanner);
 }
+
+const showWeather = (temp, windspeed) => {
+  const temperatureElement = document.querySelector(`#temperature`);
+  const windSpeedElement = document.querySelector(`#wind-speed`);
+  const windChillElement = document.querySelector(`#wind-chill`);
+
+  let chill = "n/a";
+  if (windspeed > 3 && temp <= 50) {
+    chill = Math.round(
+      35.74 +
+        0.6215 * temp -
+        35.75 * Math.pow(windspeed, 0.16) +
+        0.4275 * temp * Math.pow(windspeed, 0.16)
+    );
+  }
+
+  temperatureElement.innerHTML = temp;
+  windSpeedElement.innerHTML = windspeed;
+  windChillElement.innerHTML = chill;
+};
+
+showWeather(5, 5);
