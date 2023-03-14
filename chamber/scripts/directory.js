0bc397929ed321be47b8b9cb10a43a0740b11251
spencer-rafada <spencer.byui@gmail.com>
2023-03-14 10:34:05 -0600

    added changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -37,3 +37,15 @@ const displayBusiness = (list) => {
     contactsElement.appendChild(card);
   });
 };
+
+// Nav
+const toggleMenu = () => {
+  document.querySelector(`#navbar`).classList.toggle("menu-active");
+  document.querySelector(`#menu-open-2`).classList.toggle("menu-active");
+  document.querySelector(`#menu-close`).classList.toggle("menu-active");
+  document.querySelector(`#menu-open`).classList.toggle("menu-active");
+};
+
+// document.querySelector(`#hamburger-menu`).addEventListener(`click`, toggleMenu);
+document.querySelector(`#hamburger-menu`).onclick = toggleMenu;
+document.querySelector(`#menu-open-2`).onclick = toggleMenu;
--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -29,3 +29,15 @@ const observer = new IntersectionObserver(callback, options);
 imagesToLoad.forEach((img) => {
   observer.observe(img);
 });
+
+// Nav
+const toggleMenu = () => {
+  document.querySelector(`#navbar`).classList.toggle("menu-active");
+  document.querySelector(`#menu-open-2`).classList.toggle("menu-active");
+  document.querySelector(`#menu-close`).classList.toggle("menu-active");
+  document.querySelector(`#menu-open`).classList.toggle("menu-active");
+};
+
+// document.querySelector(`#hamburger-menu`).addEventListener(`click`, toggleMenu);
+document.querySelector(`#hamburger-menu`).onclick = toggleMenu;
+document.querySelector(`#menu-open-2`).onclick = toggleMenu;
